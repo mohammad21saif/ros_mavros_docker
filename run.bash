e24999ba7b747d10dee6d4690189809e591330f9
Mohammad Saifullah Khan <mohammad21@iiserb.ac.in>
2025-10-22 16:09:12 +0530

    Modify Docker run command for ROS Noetic
    Updated the Docker run command to use NVIDIA runtime and added additional options for IPC, PID, and X11 permissions.

--- run.bash
+++ run.bash
@@ -1,18 +1,13 @@
-#!/bin/bash
-
-# ROS Noetic Docker Container Runner
-# This script runs the ROS Noetic development container with full device access
-
-echo "Starting ROS Noetic Docker container..."
-
-docker run -it --rm \
-  -v $HOME:/home/$USER \
-  -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -e DISPLAY=$DISPLAY \
-  --network host \
-  --privileged \
-  -v /dev:/dev \
-  --name ros-noetic-container \
-  ros-noetic-dev
-
-echo "Container has stopped."
+sudo docker run -it --rm \
+    --runtime nvidia \
+    --network host \
+    --privileged \
+    --ipc host \
+    --pid host \
+    -e DISPLAY=$DISPLAY \
+    -e QT_X11_NO_MITSHM=1 \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v /dev:/dev \
+    -v $HOME/.Xauthority:/root/.Xauthority:rw \
+    --group-add dialout \
+    jetson_ros_noetic_mavros
